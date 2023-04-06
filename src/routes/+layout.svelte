6629cbf1938ef8e17414312c74f063f1e767ff80
Ricardo Viana <richardluizv@gmail.com>
2023-04-06 00:57:39 -0300

    add porject end routes and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<h1>Layout</h1>
+
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/about">Sobre</a></li>
+  <li><a href="/portifolio">Portfólio</a></li>
+  <li><a href="/contact">Contato</a></li>
+</ul>
+
+<slot />
