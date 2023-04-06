d8c834e0be89fef28469f47fead6bd1a7b7de9bc
Ricardo Viana <richardluizv@gmail.com>
2023-04-06 01:17:11 -0300

    add lib svelt-add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
+<script>
+  import "../app.postcss";
+</script>
+
 <h1>Layout</h1>
 
 <ul>
   <li><a href="/">Home</a></li>
   <li><a href="/about">Sobre</a></li>
-  <li><a href="/portifolio">Portfólio</a></li>
+  <li><a href="/portifolio">Portf&#xf3;lio</a></li>
   <li><a href="/contact">Contato</a></li>
   <li><a href="/fq">Perguntas Frequentes</a></li>
 </ul>
