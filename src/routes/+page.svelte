5e88854e1cb210f7844df4a953f814972bba4a13
Ricardo Viana <richardluizv@gmail.com>
2023-04-06 01:36:41 -0300

    add components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+    import Button from './Button.svelte';
+</script>
+
 <h1>Meu site</h1>
+
+<Button label="opa"/>
+<Button label="Clique aqui!"/>
+<Button />
---
+++ src/routes/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let label = 'Clique aqui'
+</script>
+
+<button class="bg-red-500 text-white p-3 rounded-lg">{label}</button>
+
+<style>
+    .btn {
+       color: red;
+        background-color: transparent;
+    }
+
+
+</style>
